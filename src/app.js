4e1cdceb355c5a702ebd986dc171993d4868d8c8
K <priyankakk@deloitte.com>
2022-03-21 20:55:06 +0530

    added cors policy

--- src/app.js
+++ src/app.js
@@ -15,6 +15,24 @@ const PORT = process.env.PORT || 8000;
 //app.use(logRequests); //to log requests in console
 app.use(express.json());
 console.log("test00")
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://event-management-22.herokuapp.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.use('/api/v1',userRoutes);
 app.use('/api/v1',eventsRoutes);
 app.use('/api/v1',venueRoutes);
